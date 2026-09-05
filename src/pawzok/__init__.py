648f2f18172565fede9aa08653ea6e0e8e548d81
Karthik Chandran <karthik1029@gmail.com>
2026-09-05 19:47:35 -0400

    Add public Pawzok API

---
+++ src/pawzok/__init__.py
@@ -0,0 +1,8 @@
+"""Pawzok public API."""
+
+from .exceptions import PawzokAssertionError
+from .result import SQLResult
+from .sql import assert_sql
+
+__all__ = ["assert_sql", "PawzokAssertionError", "SQLResult"]
+__version__ = "0.1.0"
