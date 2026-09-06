fc170699537b535303d2ef6f9993aec3ffce4186
Karthik Chandran <karthik1029@gmail.com>
2026-09-06 12:50:42 -0400

    Expose API assertion in public API

--- src/pawzok/__init__.py
+++ src/pawzok/__init__.py
@@ -1,8 +1,15 @@
 """Pawzok public API."""
 
+from .api import APIResult, assert_api
 from .exceptions import PawzokAssertionError
 from .result import SQLResult
 from .sql import assert_sql
 
-__all__ = ["assert_sql", "PawzokAssertionError", "SQLResult"]
+__all__ = [
+    "assert_api",
+    "assert_sql",
+    "APIResult",
+    "PawzokAssertionError",
+    "SQLResult",
+]
 __version__ = "0.1.1"
